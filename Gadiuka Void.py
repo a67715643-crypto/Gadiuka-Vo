ddcc0b0f3960039c693596391973aca51e15361a
a67715643-crypto <a67715643@gmail.com>
2026-04-20 19:36:36 +0300

    Add files via upload

---
+++ Gadiuka Void.py
@@ -0,0 +1,251 @@
+import colorama
+
+colorama.init()
+
+print(colorama.Back.LIGHTWHITE_EX)
+print(colorama.Fore.BLACK)
+
+print("""instruction:
+      print takeX2 to take;
+      print jumpX2 to jump;
+      print goX2 to go;
+      print openX2 to open;
+      print fightX2 to fight.""")
+
+print(colorama.Back.BLACK)
+print(colorama.Fore.LIGHTWHITE_EX)
+
+print('You were hurt...')
+
+print(colorama.Back.LIGHTBLACK_EX)
+print(colorama.Fore.BLACK)
+
+act = input()
+
+print('paper')
+print('take?')
+
+input()
+
+if input() == 'take':
+    print(colorama.Back.LIGHTWHITE_EX)
+    print('You created it...')
+
+    print(colorama.Back.LIGHTBLACK_EX)
+
+    print('Gadiuka\'s paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('I fou▮d some ▮nform▮▮ion about the black thing... and... YOU C▮EATED IT WHEN YOU DEFEA▮ED AD▮IN WITH MAG▮ET! IT IS YOUR C▮EATIO▮! And... You are fig▮ti▮g with it... A▮▮nd I found what is it! It... ▮t is... The V▮id. You ▮reated it and now it is ea▮▮ng our world! But the ▮orst part is t▮at YOU are it\'s nex▮ meal... We m▮st stop it!')
+
+        print(colorama.Back.LIGHTBLACK_EX)
+
+        print('paper')
+        print('take?')
+
+        input()
+
+        if input() == 'take':
+            print(colorama.Back.LIGHTWHITE_EX)
+            print('The V▮id fou▮d you!')
+
+            print(colorama.Back.LIGHTBLACK_EX)
+
+            print('mineshaft')
+            print('go?')
+
+            input()
+
+            if input() == 'go':
+                print(colorama.Fore.LIGHTWHITE_EX)
+                print(colorama.Back.BLACK)
+                print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+        print('It will find you everywhere')
+
+        print('big mineshaft')
+        print('go?')
+
+        input()
+
+        if input() == 'go':
+            print(colorama.Fore.LIGHTWHITE_EX)
+            print(colorama.Back.BLACK)
+            print("""--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------""")
+
+            print('mineshaft')
+            print('go?')
+
+            input()
+
+            if input() == 'go':
+                print(colorama.Fore.LIGHTWHITE_EX)
+                print(colorama.Back.BLACK)
+                print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+                print('mineshaft')
+                print('go?')
+
+                input()
+
+                if input() == 'go':
+                    print(colorama.Fore.LIGHTWHITE_EX)
+                    print(colorama.Back.BLACK)
+                    print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+                    print('You are caught')
+                    print('fight?')
+
+                    input()
+
+                    if input() == 'fight':
+                        print('The Void is too powerful')
+
+                    print('magnet')
+                    print('fight?')
+
+                    input()
+
+                    if input() == 'fight':
+                        print('Gadiuka: NO! DO NOT DO THIS! The magnet is just making it bigger! Just... Just RUN!')
+
+                    print('run for your life')
+                    print('go?')
+
+                    input()
+
+                    if input() == 'go':
+                        print('You escaped the Void... But Gadiuka is fighting!')
+                        print(colorama.Fore.BLACK)
+                        print(colorama.Back.LIGHTBLACK_EX)
+
+                    print('Me: Gadiuka! Get back! Void is too powerful! Gadiuka! Gadiuka! GADIUKA!')
+
+                    print('==--(knife)')
+                    print('knife')
+                    print('take?')
+
+                    input()
+
+                    if input() == 'take':
+                        print('Me: I must get back to the Void to save Gadiuka!')
+
+                    print('Void')
+                    print('go?')
+
+                    input()
+
+                    if input() == 'go':
+                        print(colorama.Back.BLACK)
+                        print(colorama.Fore.LIGHTWHITE_EX)
+                        print('Gadiuka: Use the knife to fight! I will help you!')
+
+                    print('knife')
+                    print('fight?')
+
+                    input()
+
+                    if input() == 'fight':
+                        print('Void is hurt a little.')
+
+                    print("""Gadiuka: fight
+         fight""")
+                    print('Void is hurt a little.')
+
+                    print('knife')
+                    print('fight?')
+
+                    input()
+
+                    if input() == 'fight':
+                        print('Void is hurt a little.')
+
+                        print("""Gadiuka: fight
+         fight""")
+                        print('Void is hurt a little.')
+
+                        print('knife')
+                        print('fight?')
+
+                        input()
+
+                        if input() == 'fight':
+                            print('Void is hurt a little.')
+
+                        print("""Gadiuka: fight
+         fight""")
+                        print('Void is hurt a little.')
+
+                        print('knife')
+                        print('fight?')
+
+                        input()
+
+                        if input() == 'fight':
+                            print('Void is hurt a little.')
+
+                            print("""Gadiuka: fight
+         fight""")
+                            print('Void is hurt a little.')
+
+                        print('knife')
+                        print('fight?')
+
+                        input()
+
+                        if input() == 'fight':
+                            print('Void is hurt a lot.')
+
+                        print("""Gadiuka: fight
+         fight""")
+                        print('Void is defeated.')
+
+                        print(colorama.Fore.BLACK)
+                        print(colorama.Back.LIGHTBLACK_EX)
+                        print('Gadiuka: We made it!')
+                        print('Me: Yes we done! We done with it!')
+                        print('Gadiuka: Hehe... There is nothing impossible!')
+                        print('Me: Ye!')
+                        print('Gadiuka: Yes, but now we need to get back to the Gadiuka Cafe!')
+                        print('Me: Sooo... Let\'s go')
+
+                        print('mineshaft')
+                        print('go?')
+
+                        input()
+
+                        if input() == 'go':
+                            print(colorama.Back.YELLOW)
+                            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+                        print("""
+                               _________
+                              /        /
+                             /________/
+                        """)
+                        print('trapdoor')
+                        print('go?')
+
+                        input()
+
+                        if input() == 'go':
+                            print('Trapdoor is locked...')
+
+                        print('Gadiuka: We thought the Void is the worst that can happen, but it wasn\'t...')
+
+print(colorama.Back.LIGHTWHITE_EX)
+print("""Thanks for playing!
+Made on Python
+Powered by PyCharm
+Made by Gadiuka
+
+Special thanks for Python and PyCharm""")
